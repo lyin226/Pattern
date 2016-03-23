ea603229cc8e87e7460d5c532b4303f6907fe9b4
lyin226 <liuyi_0226@yeah.net>
2016-03-23 23:37:53 +0800

    合成模式/组合模式

---
+++ pattern/src/main/java/com/composite/security/Client.java
@@ -0,0 +1,17 @@
+package com.novel.composite.security;
+
+public class Client {
+// 安全模式
+	public static void main(String[] args) {
+		Composite root = new Composite("根节点");
+		Leaf c1 = new Leaf("   叶子节点");
+		Composite c2 = new Composite("   树枝节点");
+		
+		Leaf c3 = new Leaf("        树枝节点的叶子节点");
+		root.addComponent(c1);
+		root.addComponent(c2);
+		c2.addComponent(c3);
+		
+		root.print();
+	}
+}
---
+++ pattern/src/main/java/com/composite/security/Composite.java
@@ -0,0 +1,32 @@
+package com.novel.composite.security;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//树枝节点  购物车 有多个商品
+public class Composite implements Component {
+	//用于管理树枝节点多个对象
+	private List<Component> product=new ArrayList<Component>();
+	//对象名字
+	private String name ;
+	public Composite(String name){
+		this.name=name;
+	}
+	//购物车加入一个商品
+	public void addComponent(Component testComp){
+		product.add(testComp);
+	}
+	//购物车移除一个商品
+	public void removeComponent(int index){
+		product.remove(index);
+	}
+	public void print() {
+		System.out.println("  "+name);
+		if(this.product!=null){
+			for(Component testComp:product){
+				System.out.println();//下一行
+				testComp.print();
+			}
+		}
+	}
+}
