30a15d887978cef94430335b641b128377c77036
lyin226 <liuyi_0226@yeah.net>
2016-04-05 23:14:08 +0800

    备忘录模式

---
+++ pattern/src/main/java/com/memento/Caretaker.java
@@ -0,0 +1,10 @@
+package com.memento;
+class Caretaker {  
+    private Memento memento;  
+    public Memento getMemento(){  
+        return memento;  
+    }  
+    public void setMemento(Memento memento){  
+        this.memento = memento;  
+    }  
+}  
